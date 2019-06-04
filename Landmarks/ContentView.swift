51a153f90e306d0fbd51f6d52e9ca1cb88932b5f
Chris Sanders <astericky@gmail.com>
2019-06-03 17:53:28 -0700

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Chris Sanders on 6/3/19.
+//  Copyright © 2019 Chris Sanders. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
